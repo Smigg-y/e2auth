16d8c95041414d20dfca0fd1cdcaa4e976523837
Smiggy <70798673+Smigg-y@users.noreply.github.com>
2022-09-20 20:46:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const fs = require('fs');
 const app = express();
 
 function readJson(filePath, cb) {
-    fs.readFile(filePath, (err, fileData) => {
-        if (err) {
-            return cb && cb(err);
-        };
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    };
 
-        try {
-            const object = JSON.parse(fileData);
-            return cb && cb(null, object);
-        } catch (err) {
-            return cb && cb(err);
-        };
-    });
+    try {
+      const object = JSON.parse(fileData);
+      return cb && cb(null, object);
+    } catch (err) {
+      return cb && cb(err);
+    };
+  });
 };
 
 const port = process.env.PORT || 3000;
@@ -23,31 +23,31 @@ app.set('json spaces', 4);
 app.use(express.static(`${__dirname}/`));
 app.listen(port);
 
-app.get('/auth', async function (req, res) {
-    let id = req.query.id;
-    readJson("./whitelist.json", (err, data) => {
-        if (err) {
-            console.log(err);
+app.get('/auth', async function(req, res) {
+  let id = req.query.id;
+  readJson("./whitelist.json", (err, data) => {
+    if (err) {
+      console.log(err);
 
-            res.json({
-                "allowed": false
-            });
+      res.json({
+        "allowed": false
+      });
 
-            return;
-        };
+      return;
+    };
 
-        let response = data[id] || false
-        res.json({
-            "allowed": response
-        });
-
-        if(response === true) {
-            let options = {
-                host: 'wirepusher.com',
-                path: `/send?id=pW62mps2p&title=Stolen+E2+Alert&message=${id}`
-            };
-            
-            http.request(options).end();
-        };
+    let response = data[id] || false
+    res.json({
+      "allowed": response
     });
+
+    if (response === false) {
+      var options = {
+        host: 'wirepusher.com',
+        path: `/send?id=pW62mps2p&title=Stolen+E2+Alert&message=${id}`
+      };
+
+      http.request(options).end();
+    };
+  });
 });
